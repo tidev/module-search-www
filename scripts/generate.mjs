7d9e359bddc991f4c6cb6d15d27b356969454440
Ewan Harris <ewanharris93@gmail.com>
2023-03-06 00:01:58 +0000

    feat: pull supported platforms from a best effort folder lookup

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,21 +1,32 @@
 import { Octokit } from 'octokit';
 import * as fs from 'node:fs/promises';
 
-const gh = new Octokit();
+const gh = new Octokit({
+    auth: process.env.GITHUB_TOKEN || process.env.GH_TOKEN
+});
 
 const searchResults = gh.paginate.iterator(gh.rest.search.repos, {
-    q: 'titanium in:topics language:objc language:swift language:java language:kotlin language:javascript'
+    q: 'titanium in:topics language:objc language:swift language:java language:kotlin language:javascript',
+    per_page: 100
 });
 
 const repositories = [];
 for await (const {data} of searchResults) {
-    repositories.push(...data.map(mapData));
+    const mappedData = await Promise.all(data.map(mapData));
+    repositories.push(...mappedData.filter(repo => repo));
 }
 
 const out = new URL('../data.json', import.meta.url);
 await fs.writeFile(out, JSON.stringify(repositories, undefined, '\t'));
 
-function mapData(data) {
+async function mapData(data) {
+    console.log(`mapping ${data.owner.login}/${data.name}`);
+    const platforms = await getPlatforms(data);
+
+    if (!platforms?.length) {
+        return;
+    }
+
     return {
         name: data.name,
         owner: {
@@ -24,6 +35,44 @@ function mapData(data) {
         },
         html_url: data.html_url,
         description: data.description,
-        updated_at: data.updated_at
+        updated_at: data.updated_at,
+        platforms
     }
+}
+
+async function getPlatforms (data) {
+    const platforms = [];
+    try {
+        // Check for android and ios directories. We use allSettled as this will throw an error
+        // if the folder does't exist so we check the Promise status to see whether the folder
+        // existed or not
+        const [android, ios, iphone] = await Promise.allSettled([
+            gh.rest.repos.getContent({
+                owner: data.owner.login,
+                repo: data.name,
+                path: 'android'
+            }),
+            gh.rest.repos.getContent({
+                owner: data.owner.login,
+                repo: data.name,
+                path: 'ios'
+            }),
+            gh.rest.repos.getContent({
+                owner: data.owner.login,
+                repo: data.name,
+                path: 'iphone'
+            }),
+        ]);
+    
+        if (android.status === 'fulfilled'){
+            platforms.push('android');
+        }
+    
+        if (ios.status === 'fulfilled' || iphone.status === 'fulfilled') {
+            platforms.push('ios');
+        }
+    } catch (error) {
+        return;
+    }
+    return platforms;
 }
